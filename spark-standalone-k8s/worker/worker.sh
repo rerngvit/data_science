6929dbb4502a767973745f676b66a498bb80561f
Rerngvit <rerngvit.yanggratoke@combient.com>
2019-03-10 20:16:38 +0100

    revert back to master port set from yaml

--- spark-standalone-k8s/worker/worker.sh
+++ spark-standalone-k8s/worker/worker.sh
@@ -12,4 +12,4 @@ ln -sf /dev/stdout $SPARK_WORKER_LOG/spark-worker.out
 
 echo "Starting spark worker for host at '$SPARK_MASTER_HOST' and port at '$SPARK_MASTER_PORT' "
 
-/spark/sbin/../bin/spark-class org.apache.spark.deploy.worker.Worker spark://$SPARK_MASTER_HOST:7077 >> $SPARK_WORKER_LOG/spark-worker.out
+/spark/sbin/../bin/spark-class org.apache.spark.deploy.worker.Worker spark://$SPARK_MASTER_HOST:$SPARK_MASTER_PORT >> $SPARK_WORKER_LOG/spark-worker.out
