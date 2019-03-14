e2352979e1576cc09774dcebfe629582fc8dee20
Rerngvit <rerngvit.yanggratoke@combient.com>
2019-03-14 23:12:18 +0100

    add workflow org image

---
+++ spark-standalone-k8s/workflow_org/worker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+. "/spark/sbin/spark-config.sh"
+
+. "/spark/bin/load-spark-env.sh"
+
+mkdir -p $SPARK_WORKER_LOG
+
+export SPARK_HOME=/spark
+
+# ln -sf /dev/stdout $SPARK_WORKER_LOG/spark-worker.out
+
+echo "Starting spark worker for host at '$SPARK_MASTER_HOST' and port at '$SPARK_MASTER_PORT' "
+
+/spark/sbin/../bin/spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT \
+              spark://$SPARK_MASTER_HOST:$SPARK_MASTER_PORT >> $SPARK_WORKER_LOG/spark-worker.out
