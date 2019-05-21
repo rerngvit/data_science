cbd672d0d9930ea926021620ae3875f0463ca14b
Rerngvit <rerngvit.yanggratoke@combient.com>
2019-05-21 22:44:32 +0200

    add an automation code to create a DAG folder for airflow

--- spark-k8s/pipeline/pipeline.sh
+++ spark-k8s/pipeline/pipeline.sh
@@ -4,5 +4,8 @@ airflow initdb
 airflow webserver -p 8080 &
 airflow scheduler &
 
+# Creating the DAG folder
+mkdir /root/airflow/dags
+
 export PORT=8888
 cd /data && /usr/local/bin/jupyter lab --port=$PORT --ip=0.0.0.0 --no-browser --allow-root
