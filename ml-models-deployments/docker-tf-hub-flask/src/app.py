04a88465c963db45511a658f2834179660c92476
Rerngvit <rerngvit.yanggratoke@combient.com>
2019-06-01 22:57:06 +0200

    optimizing sentence processor

--- ml-models-deployments/docker-tf-hub-flask/src/app.py
+++ ml-models-deployments/docker-tf-hub-flask/src/app.py
@@ -6,36 +6,36 @@ from text_preprocessor import TextPreprocessor
 
 # initialize our Flask application and the model
 app = flask.Flask(__name__)
-model = None
-session = None
-text_preprocessor = TextPreprocessor()
-graph = tf.Graph()
 
-def load_sentence_encoder():
-    global model, session, graph
+def init_sentence_encoder():
+    global session, graph, text_preprocessor, encoding_ops, messages_plh
+    graph = tf.Graph()
+    text_preprocessor = TextPreprocessor()
+
     print(" Start initializing Tensorflow hub")
     os.environ["TFHUB_CACHE_DIR"] = '/tf_hub_cache'
     # Create and intialize the Tensorflow session
     with graph.as_default():    
         module_url = "https://tfhub.dev/google/universal-sentence-encoder/2"
-        model = hub.Module(module_url)    
+        embed = hub.Module(module_url)    
         session = tf.Session(config=tf.ConfigProto(device_count = {'GPU': 0}))
         session.run([tf.global_variables_initializer(), tf.tables_initializer()])
+        messages_plh = tf.placeholder(dtype=tf.string, shape=[None])
+        encoding_ops = embed(messages_plh)
     print(" Model Sentence Encoder is loaded")
 
 @app.route("/sentence_encoder", methods=["POST"])
 def sentence_encoder():
-    global graph
     data = {"success": False}
     if flask.request.is_json:
         content = flask.request.get_json()
         input_txt = content["input_txt"]
+        # Disable preprocessing
         preprocessed_txt = text_preprocessor.preprocess_text(input_txt)
         data["input_text"] = input_txt
         data["preprocessed_text"] = preprocessed_txt
-
         with graph.as_default():
-            sentence_vector = session.run(model([preprocessed_txt]))[0]
+            sentence_vector = session.run(encoding_ops, feed_dict={messages_plh: [input_txt]})[0]
             array_float_str = "{%s}" % ",".join(["{:f}".format(x) for x in list(sentence_vector)])
         
         data["encoded_vector"] = array_float_str
@@ -46,5 +46,5 @@ def sentence_encoder():
 
 if __name__ == "__main__":
 	print(" Loading model and start the server...")
-	load_sentence_encoder()
+	init_sentence_encoder()
 	app.run(host='0.0.0.0', port=9090)
